beee8023192aef84f6fcaa717f700c0461206740
murai-sora <murai_sora@koushi-intec.co.jp>
2026-04-16 15:49:35 +0900

    Add files via upload

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,34 @@
+package kadai_018;
+
+abstract public class Kato_Chapter18 {
+ 
+	
+	public String familyName="加藤";//姓を表す
+	public String givenName;//名を表す
+	public String address="東京都中野区〇×";//住所を表す
+	
+	
+	//抽象クラス
+	public void commonlntroduce() {
+		
+		System.out.println("名前は"+familyName+givenName+"です");
+		System.out.println("住所は"+address+"です");
+		
+	}
+
+	abstract public void eachIntroduce();
+	
+	
+	 public void execlntroduce() {
+		commonlntroduce();
+		eachIntroduce();
+		System.out.println();
+	}
+		
+		
+}
+	
+	
+	
+	
+	
